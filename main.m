72b5883d83d018ace423fe15afe7c3e44c3cc89e
Nobuhiro Funabiki <funabiki0819@gmail.com>
2020-01-18 14:18:37 -0800

    Add the functionality to read configuration files

--- main.m
+++ main.m
@@ -15,6 +15,30 @@ addpath(genpath('../YAMLMatlab_0.4.3'));
 
 run('src/load_parameters.m');
 
+%% Instanciation
+
+% General parameters
 time_stamp      = 0.0;
 time_list       = zeros(1,num_steps);
 display_timer   = 0.0;
+
+% True states
+for iAgents = 1:num_agents
+    args_agent_true.id = iAgents;
+    args_agent_true.position = initial_states(1:num_dims, iAgents);
+    args_agent_true.velocity = initial_states(1+num_dims:2*num_dims, iAgents);
+    agents_(iAgents) = AgentHandler(args_agent_true);
+end
+
+% Reference agent
+args_agent_ref.id = 0;
+for iDims = 1:num_dims
+    args_agent_ref.position(iDims, 1) = init_state_ref.position(iDims);
+    args_agent_ref.velocity(iDims, 1) = init_state_ref.velocity(iDims);
+end
+agent_ref_ = AgentHandler(args_agent_ref);
+
+% Dynamics model
+
+
+%% Simulation
--- src/load_parameters.m
+++ src/load_parameters.m
@@ -2,8 +2,21 @@
 
 params = ReadYaml('../config/parameters.yaml');
 
+% Load files
+initial_states = readmatrix(['../config/initial_states/', params.file_name.initial_states]);
+communication_time_table = readmatrix(['../config/mesh_network/', params.file_name.mesh_network]);
+
+% General parameters
+num_agents = size(initial_states,2);
+
 % Time parameters
 total_simulation_time   = params.time.total_simulation;
 delta_time_rk           = params.time.delta_runge_kutta;
 display_period          = params.time.display_period;
-num_steps               = round(total_simulation_time/delta_time_rk);;
+num_steps               = round(total_simulation_time/delta_time_rk);
+
+% Dynamics parameters
+num_dims = params.dynamics.dimension;
+
+% Rerence agent
+init_state_ref = params.reference_agent.initial_state;

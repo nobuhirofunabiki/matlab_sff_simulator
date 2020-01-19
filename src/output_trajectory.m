3ddb20ad083cd03c84c65008a2dbce04c0df80df
Nobuhiro Funabiki <funabiki0819@gmail.com>
2020-01-18 16:11:35 -0800

    Modify the output generation file so as to create a node position file with a long time interval

--- src/output_trajectory.m
+++ src/output_trajectory.m
@@ -12,11 +12,26 @@ end
 fprintf(fid,'\n');
 fclose(fid);
 
-data = zeros(num_steps, 1+3*num_agents);
-data(:,1) = transpose(time_list);
-for iAgent = 1:num_agents
-    data(:, 1+3*(iAgent-1)+1:1+3*(iAgent-1)+3) = ...
-    transpose(v_agents_true_(iAgent).getPosition());
-end   
+% data = zeros(num_steps, 1+3*num_agents);
+% data(:,1) = transpose(time_list);
+% for iAgents = 1:num_agents
+%     data(:, 1+3*(iAgents-1)+1:1+3*(iAgents-1)+3) = ...
+%     transpose(v_agents_true_(iAgents).getPosition());
+% end
+
+data = [];
+time_delta = 10;
+for iSteps = 1:num_steps
+    if (mod(time_list(iSteps), time_delta) == 0)
+        delta_data = zeros(1,1+num_dims*num_agents);
+        delta_data(1,1) = time_list(1,iSteps);
+        for iAgents = 1:num_agents
+            positions = transpose(v_agents_true_(iAgents).getPosition());
+            delta_data(1, 1+1+num_dims*(iAgents-1):1+num_dims*iAgents) ...
+                = positions(iSteps,:);
+        end
+        data = vertcat(data, delta_data);
+    end
+end
 
 dlmwrite(['../output/', 'node_positions.csv'], data, '-append');
